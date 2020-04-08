66ff366fdc2172464939192d03875f9d12d83ff0
Radek <radek@MacBook-Pro-Radek.local>
2020-04-09 01:48:33 +0200

    Updates

---
+++ precache-manifest.ba296f5939a1d400b876bb0d81ff391a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e15bd6e665e8a5f2b703133cb31a88b6",
+    "url": "/wallpaperapp/index.html"
+  },
+  {
+    "revision": "b3bc7a15f8e42e44f8de",
+    "url": "/wallpaperapp/static/css/2.9de2e04a.chunk.css"
+  },
+  {
+    "revision": "b3bc7a15f8e42e44f8de",
+    "url": "/wallpaperapp/static/js/2.940c35fd.chunk.js"
+  },
+  {
+    "revision": "4f160e29840e1f64ec2468f8c9029bf3",
+    "url": "/wallpaperapp/static/js/2.940c35fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "691a01d7ce3e17cdfe53",
+    "url": "/wallpaperapp/static/js/main.a1b1751a.chunk.js"
+  },
+  {
+    "revision": "91c9aa899966a13e8d01",
+    "url": "/wallpaperapp/static/js/runtime-main.5df75e3d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pantryApp/precache-manifest.c633435b85e41fe31cf49cda74639156.js"
+  "/wallpaperapp/precache-manifest.ba296f5939a1d400b876bb0d81ff391a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pantryApp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wallpaperapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
